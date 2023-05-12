ca2189bbed8fb5562e062ccf651fd34123cc95fc
Liriope <zshankledonald@gmail.com>
2023-05-11 23:27:46 -0400

    Update script.js
    Fade update for menu and main page

--- script.js
+++ script.js
@@ -22,60 +22,53 @@ window.addEventListener('scroll', fadeOut);
 
 // Fade in Menu on scroll
 function fadeInMenu() {
-	// this runs every time the user scrolls
   if (window.matchMedia('(max-width: 768px)').matches) {
     return; // Exit the function and skip execution
   }
+
   var element = document.getElementById('navMenu');
-  // Calculates the vertical distance from the top of the document to the top edge of the specified element.
-  //window.pageYOffset retrieves the current vertical scroll position of the document. It represents the number of pixels that the document has been scrolled vertically from the top.
-  //element.getBoundingClientRect().top returns the distance between the top edge of the element and the top edge of the viewport, measured in pixels.
-  var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
   var elementHeight = element.offsetHeight;
-  // Retrieves the vertical scroll position of the document relative to the top of the viewport.
-	var scrollTop = document.documentElement.scrollTop;
+  var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+  var viewportHeight = window.innerHeight;
+  var scrollThreshold = distanceToTop + (0.75 * viewportHeight);
 
-	var opacity = 0;
+  var scrollTop = document.documentElement.scrollTop;
+  var opacity = 0;
 
-	if (scrollTop < distanceToTop) {
-		opacity = 1 + (scrollTop - distanceToTop) / (elementHeight * 6);
-	}
-
-  if (scrollTop > distanceToTop) {
-    opacity = 1
+  if (scrollTop > scrollThreshold) {
+    opacity = 1;
+  } else if (scrollTop > distanceToTop) {
+    opacity = (scrollTop - distanceToTop) / (elementHeight * 6);
   }
 
-	if (opacity >= 0) {
-		element.style.opacity = opacity;
-	}
+  element.style.opacity = opacity;
 }
 
 window.addEventListener('scroll', fadeInMenu);
 
-// Fade in Page on scroll
+// Fade in Menu on scroll
 function fadeInPage() {
-	// this runs every time the user scrolls
   if (window.matchMedia('(max-width: 768px)').matches) {
     return; // Exit the function and skip execution
   }
-  var element = document.getElementById('indexPageStart');
-  var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+
+  var element = document.getElementById('fadeInPage');
   var elementHeight = element.offsetHeight;
-	var scrollTop = document.documentElement.scrollTop;
+  var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+  var viewportHeight = window.innerHeight;
+  var scrollThreshold = distanceToTop + (0.75 * viewportHeight);
 
-	var opacity = 0;
+  var scrollTop = document.documentElement.scrollTop;
+  var opacity = 0;
 
-	if (scrollTop < distanceToTop) {
-		opacity = 2 + (scrollTop - distanceToTop) / (elementHeight / 15);
-	}
-
-  if (scrollTop > distanceToTop) {
-    opacity = 1
+  if (scrollTop > scrollThreshold) {
+    opacity = 1;
+  } else if (scrollTop > distanceToTop) {
+    opacity = (scrollTop - distanceToTop) / (elementHeight);
   }
 
-	if (opacity >= 0) {
-		element.style.opacity = opacity;
-	}
+  element.style.opacity = opacity;
+
 }
 
 window.addEventListener('scroll', fadeInPage);
