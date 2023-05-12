6f55c3858c2f8d69f4e21b8f6173048e00c968bc
Liriope <zshankledonald@gmail.com>
2023-05-11 23:37:51 -0400

    Update script.js
    Revert to May 9 Revision

--- script.js
+++ script.js
@@ -22,53 +22,60 @@ window.addEventListener('scroll', fadeOut);
 
 // Fade in Menu on scroll
 function fadeInMenu() {
+	// this runs every time the user scrolls
   if (window.matchMedia('(max-width: 768px)').matches) {
     return; // Exit the function and skip execution
   }
-
   var element = document.getElementById('navMenu');
-  var elementHeight = element.offsetHeight;
+  // Calculates the vertical distance from the top of the document to the top edge of the specified element.
+  //window.pageYOffset retrieves the current vertical scroll position of the document. It represents the number of pixels that the document has been scrolled vertically from the top.
+  //element.getBoundingClientRect().top returns the distance between the top edge of the element and the top edge of the viewport, measured in pixels.
   var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
-  var viewportHeight = window.innerHeight;
-  var scrollThreshold = distanceToTop + (0.75 * viewportHeight);
+  var elementHeight = element.offsetHeight;
+  // Retrieves the vertical scroll position of the document relative to the top of the viewport.
+	var scrollTop = document.documentElement.scrollTop;
 
-  var scrollTop = document.documentElement.scrollTop;
-  var opacity = 0;
+	var opacity = 0;
 
-  if (scrollTop > scrollThreshold) {
-    opacity = 1;
-  } else if (scrollTop > distanceToTop) {
-    opacity = (scrollTop - distanceToTop) / (elementHeight * 6);
+	if (scrollTop < distanceToTop) {
+		opacity = 1 + (scrollTop - distanceToTop) / (elementHeight * 6);
+	}
+
+  if (scrollTop > distanceToTop) {
+    opacity = 1
   }
 
-  element.style.opacity = opacity;
+	if (opacity >= 0) {
+		element.style.opacity = opacity;
+	}
 }
 
 window.addEventListener('scroll', fadeInMenu);
 
-// Fade in Menu on scroll
+// Fade in Page on scroll
 function fadeInPage() {
+	// this runs every time the user scrolls
   if (window.matchMedia('(max-width: 768px)').matches) {
     return; // Exit the function and skip execution
   }
-
-  var element = document.getElementById('fadeInPage');
-  var elementHeight = element.offsetHeight;
+  var element = document.getElementById('indexPageStart');
   var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
-  var viewportHeight = window.innerHeight;
-  var scrollThreshold = distanceToTop + (0.75 * viewportHeight);
+  var elementHeight = element.offsetHeight;
+	var scrollTop = document.documentElement.scrollTop;
 
-  var scrollTop = document.documentElement.scrollTop;
-  var opacity = 0;
+	var opacity = 0;
 
-  if (scrollTop > scrollThreshold) {
-    opacity = 1;
-  } else if (scrollTop > distanceToTop) {
-    opacity = (scrollTop - distanceToTop) / (elementHeight);
+	if (scrollTop < distanceToTop) {
+		opacity = 2 + (scrollTop - distanceToTop) / (elementHeight / 15);
+	}
+
+  if (scrollTop > distanceToTop) {
+    opacity = 1
   }
 
-  element.style.opacity = opacity;
-
+	if (opacity >= 0) {
+		element.style.opacity = opacity;
+	}
 }
 
 window.addEventListener('scroll', fadeInPage);
